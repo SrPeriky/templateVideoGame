2a9af4d0388ccdbab2ac96409587ba2d0f4d811c
SrPeriky <periky22@gamil.com>
2023-02-01 21:21:06 -0500

    se documenta la clase FPSCounter

--- html 2D/js/FPSCounter.class.js
+++ html 2D/js/FPSCounter.class.js
@@ -1,15 +1,35 @@
 /**
- * 
+ * Clase que se encarga de calcular y mostrar los FPS (fotogramas por segundo) en un juego o aplicación en tiempo real.
  * @class
  */
 export default class FPSCounter {
+  /**
+   * Crea una nueva instancia de FPSCounter.
+   */
   constructor() {
+    /**
+     * Almacena el valor actual de los FPS.
+     * @type {number}
+     */
     this.fps = 0;
+
+    /**
+     * Almacena el número de frames renderizados durante el último segundo.
+     * @type {number}
+     */
     this.frames = 0;
-    this.frames = 0;
+
+    /**
+     * Almacena el tiempo en milisegundos desde que se inició la aplicación.
+     * @type {number}
+     */
     this.time = performance.now();
   }
 
+  /**
+   * Calcula los FPS y actualiza el valor de "fps".
+   * @returns {number} - Los FPS actuales.
+   */
   calculateFPS() {
     this.frames++;
     const currentTime = performance.now();
@@ -22,12 +42,16 @@ export default class FPSCounter {
   }
 
   /**
-  * Dibuja los FPS en el canvas
-  * @param {CanvasRenderingContext2D} ctx - Contexto del canvas
-  */
-  draw(ctx) {
-    ctx.fillStyle = "black";
-    ctx.font = "16px Arial";
-    ctx.fillText(`FPS: ${this.fps}`, 10, 20);
+   * Dibuja los FPS en el contexto de canvas especificado.
+   * @param {CanvasRenderingContext2D} ctx - El contexto de canvas en el que se dibujarán los FPS.
+   * @param {number} [x=10] - La posición horizontal en pixels desde el borde izquierdo del canvas en el que se dibujarán los FPS.
+   * @param {number} [y=20] - La posición vertical en pixels desde el borde superior del canvas en el que se dibujarán los FPS.
+   * @param {string} [baseColor="black"] - El color del texto que se utilizará para dibujar los FPS.
+   * @param {string} [font="16px Arial"] - La fuente que se utilizará para dibujar los FPS.
+   */
+  draw(ctx, x = 10, y = 20, baseColor = "black", font = "16px Arial") {
+    ctx.fillStyle = baseColor;
+    ctx.font = font;
+    ctx.fillText(`FPS: ${this.fps}`, x, y);
   }
 }
